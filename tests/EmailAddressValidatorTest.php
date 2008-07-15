f2ae8ebe298059b8e3f931e3f14b60613b5de56b
dave@addedbytes.com <dave@addedbytes.com@6edb0c33-eb51-0410-810f-ad4e99340769>
2008-07-15 11:01:29 +0000

    Moved addresses into separated tests to make it easier to spot which addresses failed.
    git-svn-id: http://php-email-address-validation.googlecode.com/svn/trunk@3 6edb0c33-eb51-0410-810f-ad4e99340769

--- tests/EmailAddressValidatorTest.php
+++ tests/EmailAddressValidatorTest.php
@@ -10,50 +10,6 @@
 
         protected $objValidator = null;
 
-        // List of valid addresses according to standards
-        private $arrValidAddresses = array(
-             // Common email address types
-                 'test@example.com' // Standard
-                ,'TEST@example.com' // Upper case local part
-                ,'1234567890@example.com' // Numeric local part
-                ,'test+test@example.com' // Tagged "test"
-                ,'test-test@example.com' // Tagged (qmail format)
-             // Different local parts
-                ,'t*est@example.com' // Unusual characters
-                ,'+1~1+@example.com'
-                ,'{_dave_}@example.com'
-                ,'"[[ dave ]]"@example.com' // Quoted local part
-                ,'test.test@example.com' // Dot-separated local part
-                ,'dave."dave"@example.com' // Obsolete label (combination)
-                ,'"test@test"@example.com' // "@" symbol in quoted local part
-             // Different domain parts
-                ,'test@123.123.123.123' // IP address
-                ,'test@[123.123.123.123]' // IP in square brackets
-                ,'test@example.example.com' // Multiple domain labels
-            );
-
-        // List of invalid addresses according to standards
-        private $arrInvalidAddresses = array(
-             // Different local parts
-                 'test.example.com' // No "@" symbol
-                ,'test.@example.com' // Period is a seperator in local part
-                ,'test@test@example.com' // Multiple "@" symbols.
-                ,'test@@example.com' // Multiple adjacent "@" symbols.
-                ,'test..test@example.com' // Local part sections cannot be blank
-                ,'-- test --@example.com' // No spaces allowed in local part
-                ,'[test]@example.com' // Square brackets only allowed within quotes
-                ,'.test@example.com' // Local part cannot start with a period
-                ,'12345678901234567890123456789012345678901234567890123456789012345@example.com' // Local part should not be over 64 chars (this is 65)
-                ,'"test\test"@example.com' // Quotes cannot contain a backslash
-                ,'"test"test"@example.com' // Quotes cannot contain more quotes
-                ,'()[]\;:,<>@example.com' // Invalid characters in local part
-             // Different domain parts
-                ,'test@.' // Domain labels must be at least 1 character
-                ,'test@example' // Domain must contain at least 2 lables
-                ,'test@[123.123.123.123' // Unpartnered square bracket
-                ,'test@123.123.123.123]'
-            );
-
         public function setUp() {
             $this->objValidator = new EmailAddressValidator;
         }
@@ -62,19 +18,109 @@
             unset($this->objValidator);
         }
 
-        // Test the valid addresses.
-        public function testValidAddresses() {
-            foreach ($this->arrValidAddresses as $strAddress) {
-                $this->assertEquals(true, $this->objValidator->check_email_address($strAddress));
-            }
+        /* Valid Addresses
+        -------------------- */
+
+        public function testValidAddress_Standard() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test@example.com'));
         }
 
-        // Test the invalid addresses.
-        public function testInvalidAddresses() {
-            foreach ($this->arrInvalidAddresses as $strAddress) {
-                $this->assertEquals(false, $this->objValidator->check_email_address($strAddress));
-            }
+        public function testValidAddress_UpperCaseLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('TEST@example.com'));
         }
+
+        public function testValidAddress_NumericLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('1234567890@example.com'));
+        }
+
+        public function testValidAddress_TaggedLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test+test@example.com'));
+        }
+
+        public function testValidAddress_QmailLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test-test@example.com'));
+        }
+
+        public function testValidAddress_UnusualCharactersInLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('t*est@example.com'));
+            $this->assertEquals(true, $this->objValidator->check_email_address('+1~1+@example.com'));
+            $this->assertEquals(true, $this->objValidator->check_email_address('{_test_}@example.com'));
+        }
+
+        public function testValidAddress_QuotedLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('"[[ test ]]"@example.com'));
+        }
+
+        public function testValidAddress_AtomisedLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test.test@example.com'));
+        }
+
+        public function testValidAddress_ObsoleteLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test."test"@example.com'));
+        }
+
+        public function testValidAddress_QuotedAtLocalPart() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('"test@test"@example.com'));
+        }
+
+        public function testValidAddress_IpDomain() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test@123.123.123.123'));
+        }
+
+        public function testValidAddress_BracketIpDomain() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test@[123.123.123.123]'));
+        }
+
+        public function testValidAddress_MultipleLabelDomain() {
+            $this->assertEquals(true, $this->objValidator->check_email_address('test@example.example.com'));
+            $this->assertEquals(true, $this->objValidator->check_email_address('test@example.example.example.com'));
+        }
+
+        /* Invalid Addresses
+        -------------------- */
+
+        public function testInvalidAddress_NoAtSymbol() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('test.example.com'));
+        }
+
+        public function testInvalidAddress_BlankAtomInLocalPart() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('test.@example.com'));
+            $this->assertEquals(false, $this->objValidator->check_email_address('test..test@example.com'));
+            $this->assertEquals(false, $this->objValidator->check_email_address('.test@example.com'));
+        }
+
+        public function testInvalidAddress_MultipleAtSymbols() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@test@example.com'));
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@@example.com'));
+        }
+
+        public function testInvalidAddress_InvalidCharactersInLocalPart() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('-- test --@example.com')); // No spaces allowed in local part
+            $this->assertEquals(false, $this->objValidator->check_email_address('[test]@example.com')); // Square brackets only allowed within quotes
+            $this->assertEquals(false, $this->objValidator->check_email_address('"test\test"@example.com')); // Quotes cannot contain backslash
+            $this->assertEquals(false, $this->objValidator->check_email_address('"test"test"@example.com')); // Quotes cannot be nested
+            $this->assertEquals(false, $this->objValidator->check_email_address('()[]\;:,<>@example.com')); // Disallowed Characters
+        }
+
+        public function testInvalidAddress_DomainLabelTooShort() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@.'));
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@example.'));
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@.org'));
+        }
+
+        public function testInvalidAddress_DomainLabelTooLong() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012.com')); // 255 characters is maximum length for domain. This is 256.
+        }
+
+        public function testInvalidAddress_TooFewLabelsInDomain() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@example'));
+        }
+
+        public function testInvalidAddress_UnpartneredSquareBracketIp() {
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@[123.123.123.123'));
+            $this->assertEquals(false, $this->objValidator->check_email_address('test@123.123.123.123]'));
+        }
+
     }
 
 ?>
